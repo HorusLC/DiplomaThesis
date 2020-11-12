89012e43609ba2191a913044a6007e312c82a2d7
HorusLC <dkostadinov_11@abv.com>
2020-11-12 22:16:46 +0200

    changed label mode in loading to binary

--- datamgmt.py
+++ datamgmt.py
@@ -1,12 +1,56 @@
 from tensorflow import keras as krs
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import cv2 as cv
-
+import matplotlib.pyplot as plotter
+import numpy
 image_size = (256, 256)
 batch_size = 8
 
 # def add_augmentation():
 
+
+def plot_dict_accuracy(history):                      # uses dictionary as input
+    plotter.plot(history['accuracy'])
+    plotter.plot(history['val_accuracy'])
+    plotter.title('accuracy of model advanced xception')
+    plotter.ylabel('accuracy')
+    plotter.xlabel('epoch')
+    plotter.legend(['train', 'val'], loc='upper left')
+    plotter.savefig('history_acc.png')
+    plotter.show()
+
+
+def plot_history_accuracy(history):                    #uses history object as input (returned by model.fit())
+    plotter.plot(history.history['accuracy'])
+    plotter.plot(history.history['val_accuracy'])
+    plotter.title('accuracy of model advanced xception')
+    plotter.ylabel('accuracy')
+    plotter.xlabel('epoch')
+    plotter.legend(['train', 'val'], loc='upper left')
+    plotter.savefig('history_acc.png')
+    plotter.show()
+
+
+def plot_dict_loss_func_(history):
+    plotter.plot(history['loss'])
+    plotter.plot(history['val_loss'])
+    plotter.title('model loss')
+    plotter.ylabel('loss')
+    plotter.xlabel('epoch')
+    plotter.legend(['train', 'val'], loc='upper left')
+    plotter.savefig('history_lossval.png')
+    plotter.show()
+
+
+def plot_history_loss_func(history):
+    plotter.plot(history.history['loss'])
+    plotter.plot(history.history['val_loss'])
+    plotter.title('model loss')
+    plotter.ylabel('loss')
+    plotter.xlabel('epoch')
+    plotter.legend(['train', 'val'], loc='upper left')
+    plotter.savefig('history_lossval.png')
+    plotter.show()
 
 
 def load_validation(path):
@@ -16,6 +60,7 @@ def load_validation(path):
         validation_split=0.2,
         subset='validation',
         seed=1333,
+        labels='inferred',
         image_size=image_size,
         batch_size=batch_size,
     )
@@ -50,28 +95,46 @@ def load_test_data(path):
     )
     return loaded
 
+def load_viz(path):
+    validation_data = krs.preprocessing.image_dataset_from_directory(
+        path,
+        color_mode='grayscale',
+        validation_split=0.2,
+        subset='validation',
+        seed=1333,
+        image_size=image_size,
+        batch_size=batch_size,
+        label_mode='binary'
+    )
+    return  validation_data
+
+
+
+
+
 
 def load_dataset_with_visualization(path):
     loaded = krs.preprocessing.image_dataset_from_directory(
         path,
         labels='inferred',
         color_mode='grayscale',
+        subset="validation",
         batch_size=batch_size,
         image_size=image_size,
         shuffle=True,
         seed=1333,
-        validation_split=0,
+        validation_split=0.2,
     )
-    cv.namedWindow('image', cv.WINDOW_NORMAL)
-    cv.resizeWindow('image', 600, 600)
+    #cv.namedWindow('image', cv.WINDOW_NORMAL)
+    #cv.resizeWindow('image', 600, 600)
     plt.figure(figsize=(10, 10))
     for images, labels in loaded.take(1):
         for i in range(9):
             ax = plt.subplot(3, 3, i + 1)
-            plt.imshow(images[i])
+            plt.imshow(images[i].numpy().astype('uint8'))
             # a= images[i].numpy().astype("uint8")
-            cv.imshow('image', images[i].numpy().astype("uint8"))
-            cv.waitKey(0)
+            #cv.imshow('image', images[i].numpy().astype("uint8"))
+            #cv.waitKey(0)
             plt.title(int(labels[i]))
             plt.axis("off")
     plt.waitforbuttonpress(0)
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import modelcreation as mdl
 from tensorflow import keras as keras
 import datetime as dt
 import time as ti
+import pickle as pickle
+import json as json
+import matplotlib as plotter
+import pandas as panda
 
 TRAINING_PATH = 'E:/kaggle/chest_xray_imgs_pneumonia/archive/chest_xray/train'
 TEST_PATH = 'E:/kaggle/chest_xray_imgs_pneumonia/archive/chest_xray/test'
@@ -12,28 +16,43 @@ def train_model_on_data():
     training = datalib.load_dataset(TRAINING_PATH)
     val_ds = datalib.load_validation(TRAINING_PATH)
     model = mdl.model_create_adv_xception(input_shape=datalib.image_size + (1,))
-    epochs = 25
+    epochs = 45
 
     # keras.backend.clear_session()
     callbacks = [
-        keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+        keras.callbacks.ModelCheckpoint("xc2_rms_nds/save_at_{epoch}.h5"),
+        # keras.callbacks.CSVLogger(filename="xception_log.csv", separator=',', append=True)
     ]
     model.compile(
-        optimizer=keras.optimizers.Adam(1e-3),
+        optimizer=keras.optimizers.RMSprop(learning_rate=0.0001),
         loss="binary_crossentropy",
         metrics=["accuracy", "AUC",
-                 keras.metrics.TruePositives,
-                 keras.metrics.FalsePositives,
-                 keras.metrics.TrueNegatives,
-                 keras.metrics.FalseNegatives]
+                 keras.metrics.TruePositives(),
+                 keras.metrics.FalsePositives(),
+                 keras.metrics.TrueNegatives(),
+                 keras.metrics.FalseNegatives()]
     )
-    model.fit(
+
+    history = model.fit(
         training, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
     )
+    #dump results binary andjson
+    with open('xc2_rms_nds/rms_history', mode='wb') as history_file:
+        pickle.dump(history.history, history_file)
+    with open('xc2_rms_nds/rms_hist.json', mode='w') as file:
+        dataframe = panda.DataFrame(history.history)
+        dataframe.to_json(file)
+
+    #plot after all epochs
+    datalib.plot_history_accuracy(history)
+    datalib.plot_history_loss_func(history)
+
+def eval_single_model(test_data,path):
+    mod = keras.models.load_model(path)
+    print(str(mod.evaluate(test_data, verbose=1)))
 
 
 def eval_model(test_data):
-
     output_file = str(ti.strftime('%Y-%m-%d-%H%M%S')) + '.txt'
     with open(output_file, mode='a') as f:
         for i in range(36, 46, 1):  # from 1 to 8(not included) with a step of 1
@@ -46,16 +65,37 @@ def eval_model(test_data):
     # results = mod.evaluate(test_data, verbose=1)
 
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    t_data = datalib.load_test_data(TEST_PATH)
-    eval_model(t_data)
+    #val= datalib.load_validation(TRAINING_PATH)
+    a=datalib.load_viz(TRAINING_PATH)
+    print('a')
+    # datalib.load_dataset_with_visualization(TRAINING_PATH)
+    #train_model_on_data()
+    #t_data = datalib.load_test_data(TEST_PATH)
+    # eval_single_model(t_data, path='xception_2_rmsprop/save_at_40.h5')
+    # eval_model(t_data)
     # training = datalib.load_dataset(TRAINING_PATH)
     # val_ds = datalib.load_validation('E:/kaggle/chest_xray_imgs_pneumonia/archive/chest_xray/train')
-    # model = keras.models.load_model("save_ep_35.h5")
+    # model = keras.models.load_model("xception_2_rmsprop/save_at_21.h5")
     # callbacks = [
-    #     keras.callbacks.ModelCheckpoint("save_ep_{epoch}.h5"),
-    # ]
+    #      keras.callbacks.ModelCheckpoint("xception_2_rmsprop/save_at_{epoch}.h5"),
+    #      # keras.callbacks.CSVLogger(filename="xception_log.csv", separator=',', append=True)
     #
-    # model.fit(training, initial_epoch=35, epochs=45, callbacks=callbacks, validation_data=val_ds)
+    #  ]
+    # history = model.fit(training, initial_epoch=21, epochs=45, callbacks=callbacks, validation_data=val_ds)
+    # with open('xception_2_rmsprop/rms_hist.json', mode='r') as file:
+    #     hist_df = panda.read_json(file)
+    #     print(hist_df)
+    #     hist_df.plot(use_index=True, y=['loss', 'val_loss'], xticks=range(1,45,1))
+    #     plotter.pyplot.show()
+       # datalib.plot_dict_accuracy(hist_df.to_dict())
+       #datalib.plot_dict_loss_func(hist_df.to_dict())
+    #     new_df = panda.DataFrame(history.history)
+    #     updated_df = hist_df.append(new_df, ignore_index=True)
+    # with open('xception_2_rmsprop/rms_hist.json', mode='w') as f:
+    #     updated_df.to_json(f)
+#  print(model.optimizer.learning_rate)
+# history = model.fit(training, initial_epoch=65, epochs=75, callbacks=callbacks, validation_data=val_ds)
+# history = panda.read_csv('xception_log.csv', sep=',', engine='python')
+# datalib.plot_history_accuracy(history)
+# datalib.plot_history_loss_func(history)
